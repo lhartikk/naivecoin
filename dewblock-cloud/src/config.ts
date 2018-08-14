28afbccb2e65200a9e124d9b9e7a542014c434e6
ywang <ywang@upei.ca>
2018-08-13 20:31:20 -0400

    config files

--- dewblock-cloud/src/config.ts
+++ dewblock-cloud/src/config.ts
@@ -1,5 +1,5 @@
-//let dewAddress = '127.0.0.1:6001';
-let dewAddress = '192.168.1.186:6001';
+let dewAddress = '127.0.0.1:6001';
+//let dewAddress = '192.168.1.186:6001';
 //let dewAddress = '192.168.1.114:6001';
 //If dew address is not correct, it can also be set using HTTP command. See Readme.
 //Please notice: if only one machine is involved in testing, default config files are OK. 
